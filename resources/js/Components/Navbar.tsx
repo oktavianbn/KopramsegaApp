1591413aeb5218c024f04949011b4cadd37f072f
Rizafii <firmansyah.riza6@smk.belajar.id>
2025-10-30 20:12:19 +0700

    delete user

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -46,55 +46,13 @@ export function Navbar({ className, onMenuToggle }: NavbarProps) {
                 {/* Right side - Notifications and User */}
                 <div className="flex items-center gap-4">
                     {/* Notifications */}
-                    <button className="relative p-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-lg transition-colors">
-                        <Bell className="h-5 w-5" />
+                    <button className="relative p-2">
+                        <Bell className="h-5 w-5 text-white" />
                         {/* Notification badge */}
                         <span className="absolute -top-1 -right-1 h-4 w-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
                             3
                         </span>
                     </button>
-
-                    {/* User Menu */}
-                    <div className="relative">
-                        <Dropdown>
-                            <Dropdown.Trigger>
-                                <span className="inline-flex rounded-md">
-                                    <button
-                                        type="button"
-                                        className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
-                                    >
-                                        {user.name}
-
-                                        <svg
-                                            className="-me-0.5 ms-2 h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </Dropdown.Trigger>
-
-                            <Dropdown.Content>
-                                <Dropdown.Link href={route("profile.edit")}>
-                                    Profile
-                                </Dropdown.Link>
-                                <Dropdown.Link
-                                    href={route("logout")}
-                                    method="post"
-                                    as="button"
-                                >
-                                    Log Out
-                                </Dropdown.Link>
-                            </Dropdown.Content>
-                        </Dropdown>
-                    </div>
                 </div>
             </div>
         </nav>
